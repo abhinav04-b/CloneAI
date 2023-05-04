71e7e9cacc814b48aa93cfa2fad10ab37029adbb
abhinav04-b <76025844+abhinav04-b@users.noreply.github.com>
2023-05-05 00:37:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# CloneAI
-An AI which works on web page and created with flask and chatgpt
+The Flask app is a web-based chatbot that uses OpenAI's GPT-3 language model to provide responses to user input. It consists of a server and a web page that allows users to input text and receive responses from the chatbot. The server processes incoming requests and sends user input to the GPT-3 model, while the web page displays the input form and chatbot responses. The purpose of the app is to provide a simple way for users to interact with an AI-powered chatbot and receive responses to their questions or inputs.
+
+set-up: 1.Don't have have to anything just go to openai api and make a api key and paste it in 4th line of main.py code
+2.now exucte comand "python .\main.py" or simply run the main.py file.
+3.go to the given domain and enjoy.
+
+You can ask everything regarding everthing, exept which may voilance the community. 
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, redirect
+import openai
+
+openai.api_key = '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'
+
+server = Flask(__name__)
+
+def send_gpt(prompt):
+    try:
+        response = openai.Completion.create(
+        engine='davinci',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return e
+
+@server.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if len(request.form['question']) < 1:
+            return render_template(
+                'home.html', question="NULL", res="Question can't be empty!")
+        question = request.form['question']
+        print("======================================")
+        print("Receive the question:", question)
+        res = send_gpt(question)
+        print("Q：\n", question)
+        print("A：\n", res)
+
+        return redirect('/results?question={}&res={}'.format(question, res))
+    return render_template('home.html', question=0)
+
+@server.route('/results')
+def results():
+    question = request.args.get('question')
+    res = request.args.get('res')
+    return render_template('results.html', question=question, res=res)
+
+if __name__ == '__main__':
+    server.run(debug=True, host='0.0.0.0', port=80)
